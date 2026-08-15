6e789371984295a4d2a7ed5bf8df3edb3bba369e
peterGwapo29 <peterolanolan@gmail.com>
2026-08-15 23:57:05 +0800

    Fix Vite build for production

--- Backend/sumbong-app-backend/start.sh
+++ Backend/sumbong-app-backend/start.sh
@@ -6,10 +6,6 @@ php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-php artisan migrate --force
-
-php artisan db:seed --force
-
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
